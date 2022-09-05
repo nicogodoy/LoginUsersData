d0b9ff000a2bbf964efe6b0d3180136ce1e975b1
nico <nicogodoycvu10@gmail.com>
2022-09-05 16:21:33 -0300

    foother con grid y limpieza de codigo

--- src/foother/index.js
+++ src/foother/index.js
@@ -11,10 +11,10 @@ function Foother() {
     <>
       <div className="footherCont">
         <div className="boxOne">
-        <h4 className="titleContain">
+          <h4 className="titleContain">
             Siguenos en nuestras redes para mas informacion
           </h4>
-          <ul >
+          <ul>
             <li className="icon">
               <AiOutlineFacebook />
             </li>
@@ -31,88 +31,14 @@ function Foother() {
           </ul>
         </div>
         <div className="boxTwo">
-        <h4 className="title">Sobre nosotros:</h4>
+          <h4 className="title">Sobre nosotros:</h4>
           <p>
             Somos una empresa que se dedica a la manipulación de infomacion de
             usuarios
           </p>
         </div>
-        
-        {/* <div className="about">
-          <h4>Sobre nosotros</h4>
-          <p>
-            Somos una empresa que se dedica a la manipulación de infomacion de
-            usuarios
-          </p>
-        </div>
-        <div className="footherSocial">
-          <h4 className="titleh4">
-            Siguenos en nuestras redes para mas informacion
-          </h4>
-          <ul>
-            <li className="iconLetter">
-              <AiOutlineFacebook />
-            </li>
-
-            <li className="iconLetter">
-              <AiOutlineInstagram />
-            </li>
-            <li className="iconLetter">
-              <AiOutlineLinkedin />
-            </li>
-            <li className="iconLetter">
-              <AiOutlineWhatsApp />
-            </li>
-          </ul>
-        </div> */}
       </div>
     </>
   );
 }
 export default Foother;
-
-
-// import {
-//   AiOutlineFacebook,
-//   AiOutlineInstagram,
-//   AiOutlineLinkedin,
-//   AiOutlineWhatsApp,
-// } from "react-icons/ai";
-// import "./foother.css";
-
-// function Foother() {
-//   return (
-//     <>
-//       <div className="footherCont">
-//         <div className="about">
-//           <h4>Sobre nosotros</h4>
-//           <p>
-//             Somos una empresa que se dedica a la manipulación de infomacion de
-//             usuarios
-//           </p>
-//         </div>
-//         <div className="footherSocial">
-//           <h4 className="titleh4">
-//             Siguenos en nuestras redes para mas informacion
-//           </h4>
-//           <ul>
-//             <li className="iconLetter">
-//               <AiOutlineFacebook />
-//             </li>
-
-//             <li className="iconLetter">
-//               <AiOutlineInstagram />
-//             </li>
-//             <li className="iconLetter">
-//               <AiOutlineLinkedin />
-//             </li>
-//             <li className="iconLetter">
-//               <AiOutlineWhatsApp />
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-// export default Foother;
